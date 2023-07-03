28c84d56f1370f002a5f0a74f01df55d864cfc19
yuki <yuki.laurentiu@gmail.com>
2023-07-02 18:19:44 -0600

    made a function of widget list

--- lib/screen2.dart
+++ lib/screen2.dart
@@ -13,65 +13,48 @@ class Screen2 extends StatefulWidget {
 class _Screen2State extends State<Screen2> {
   @override
   Widget build(BuildContext context) {
-    List hairimg = [
-      'images/hair1.jpg',
-      'images/hair2.jpg',
-      'images/hair3.jpg',
-      'images/hair4.jpg'
-    ];
-
-    const double picPadding = 5.0;
-    final double picSize =
-        (MediaQuery.of(context).size.width / 3) - 2 * picPadding;
-
     return Scaffold(
       appBar: navBar(context),
       backgroundColor: Colors.red,
       body: Column(
         children: [
           Wrap(
-            children: [
-              SizedBox(),
-              Container(
-                padding: EdgeInsets.fromLTRB(0, picPadding, picPadding, 0),
-                child: Image.asset(
-                  'images/hair1.jpg',
-                  fit: BoxFit.cover,
-                  width: picSize,
-                  height: picSize,
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.fromLTRB(0, picPadding, picPadding, 0),
-                child: Image.asset(
-                  'images/hair2.jpg',
-                  fit: BoxFit.cover,
-                  width: picSize,
-                  height: picSize,
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.fromLTRB(0, picPadding, picPadding, 0),
-                child: Image.asset(
-                  'images/hair3.jpg',
-                  fit: BoxFit.cover,
-                  width: picSize,
-                  height: picSize,
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(0, picPadding, picPadding, 0),
-                child: Image.asset(
-                  'images/hair4.jpg',
-                  fit: BoxFit.cover,
-                  width: picSize,
-                  height: picSize,
-                ),
-              ),
-            ],
+            children: card(context),
           ),
         ],
       ),
     );
   }
+}
+
+List<Widget> card(context) {
+  List hairImg = [
+    'images/hair1.jpg',
+    'images/hair2.jpg',
+    'images/hair3.jpg',
+    'images/hair4.jpg'
+  ];
+
+  const double picPadding = 5.0;
+  final double picSize =
+      (MediaQuery.of(context).size.width / 3) - 2 * picPadding;
+
+  List<Widget> result = [];
+  for (var imgPath in hairImg) {
+    result.add(
+      GestureDetector(
+        onTap: (){},
+        child: Container(
+          padding: const EdgeInsets.fromLTRB(picPadding, picPadding, picPadding, 0),
+          child: Image.asset(
+            imgPath,
+            fit: BoxFit.cover,
+            width: picSize,
+            height: picSize,
+          ),
+        ),
+      ),
+    );
+  }
+  return result;
 }
